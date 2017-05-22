59731d8a996df14e531ccc9710b9bb1be65f3deb
zacrobat <zacrobat55@gmail.com>
2017-05-23 08:58:32 +1000

    First commit

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+console.log("hi there, amigo tres!");
+
+console.log("yo");
+
+console.log("dre");
+
+console.log("kick in the bass!");
---
+++ src/js/buildNotes.js
@@ -0,0 +1,7 @@
+// This file is ignored because it's listed in .eslintignore
+// https://css-tricks.com/why-npm-scripts/
+// "scripts": {
+//     "build:js": "npm run lint &amp;&amp; npm run uglify",
+//     "build:images": "npm run imagemin && npm run icons",
+//     "build:all": "npm run build:css && npm run build:js && npm run build:images",
+// }
